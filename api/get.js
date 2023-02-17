a78c59d070b757987687a67c200a5edf11ee15f2
mj6meron <meronmkl@gmail.com>
2023-02-17 17:18:34 +0100

    worked with sockets

--- api/get.js
+++ api/get.js
@@ -1,52 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
-
-
 // Define routes for /, /door, /window, and /lamp APIs
-router.get("/", function (req, res) {
-  console.log("Request received at /:");
-  if (!req) {
-    res.send("You succefully send a request and request body: ", req);
-  } else {
+function getRoutes(io) {
+  router.get("/", function (req, res) {
+    console.log("Request received at /:");
     res.send(`This is the root API.`);
-  }
-});
+  });
 
-router.get("/door", function (req, res) {
-  console.log("Request received at /door:");
-  if (!req) {
-    res.send("You succefully send a request and request body: ", req);
-  } else {
+  router.get("/door", function (req, res) {
+    console.log("Request received at /door:");
     res.send(`This is the door API.`);
-  }
-});
+  });
 
-router.get("/window", function (req, res) {
-  console.log("Request received at /window:");
-  if (!req) {
-    res.send("You succefully send a request and request body: ", req);
-  } else {
+  router.get("/window", function (req, res) {
+    console.log("Request received at /window:");
     res.send(`This is the window API.`);
-  }
-});
+  });
 
-router.get("/lamp/on", function (req, res) {
-  console.log("Request received at /lamp: on");
-  if (!req) {
-    res.send("You succefully send a request to turn on lampaaa");
-  } else {
-    res.send("You succefully send a request to turn on lampaaa");
-  }
-});
-router.get("/lamp/off", function (req, res) {
-  console.log("Request received at /lamp: off");
-  if (!req) {
-    res.send("You succefully send a request to turn off lampaaa");
-  } else {
-    res.send("You succefully send a request to turn off lampaaa");
-  }
-});
+  router.get("/lamp/on", function (req, res) {
+    console.log("Request received at /lamp: on");
+    // Send a message to the Socket.io server to turn the lamp on
+    io.emit('lamp:on', { data: 'on' });
+    res.send("Turning on lamp...");
+  });
 
+  router.get("/lamp/off", function (req, res) {
+    console.log("Request received at /lamp: off");
+    // Send a message to the Socket.io server to turn the lamp off
+    io.emit('lamp:off', { data: 'off' });
+    res.send("Turning off lamp...");
+  });
 
-module.exports = router;
+  return router;
+}
+
+module.exports = getRoutes;
+
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 var cors = require("cors");
+const http = require('http');
+const socketio = require('socket.io');
+const getRouter = require("./api/get");
 
 const app = express();
 
@@ -13,14 +16,39 @@ app.use(cors()); // Use this after the variable declaration
 // Connect to MongoDB using Mongoose
 // This will be implemeted shortly
 
-// Use the GET and POST routers
-const getRouter = require("./api/get");
-const postRouter = require("./api/post");
-app.use("/", getRouter);
-app.use("/", postRouter);
+// Create HTTP server and attach Socket.io
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Handle socket connection
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle incoming messages from the client
+  socket.on('lamp:on', (data) => {
+    console.log('Received lamp:on message');
+    // Send a message back to the client to acknowledge the request
+    socket.emit('lamp:status', { status: 'on' });
+  });
+
+  socket.on('lamp:off', (data) => {
+    console.log('Received lamp:off message');
+    // Send a message back to the client to acknowledge the request
+    socket.emit('lamp:status', { status: 'off' });
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Use the GET router
+app.use("/", getRouter(io));
 
 // Start the server
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+server.listen(port, function () {
   console.log(`Server is listening on port ${port}`);
 });
+
