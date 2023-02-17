77cf0bcbefa1aac04b1c0bb98cd29aa726c475af
mj6meron <meronmkl@gmail.com>
2023-02-17 18:01:11 +0100

    working with emit

--- api/get.js
+++ api/get.js
@@ -21,14 +21,14 @@ function getRoutes(io) {
   router.get("/lamp/on", function (req, res) {
     console.log("Request received at /lamp: on");
     // Send a message to the Socket.io server to turn the lamp on
-    io.emit('lamp:on', { data: 'on' });
+    io.emit("lamp:on", { data: "on" });
     res.send("Turning on lamp...");
   });
 
   router.get("/lamp/off", function (req, res) {
     console.log("Request received at /lamp: off");
     // Send a message to the Socket.io server to turn the lamp off
-    io.emit('lamp:off', { data: 'off' });
+    io.emit("lamp:off", { data: "off" });
     res.send("Turning off lamp...");
   });
 
@@ -36,4 +36,3 @@ function getRoutes(io) {
 }
 
 module.exports = getRoutes;
-
--- server.js
+++ server.js
@@ -1,54 +1,55 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const http = require("http");
+const socketio = require("socket.io");
+const getRoutes = require("./api/get");
 var cors = require("cors");
-const http = require('http');
-const socketio = require('socket.io');
-const getRouter = require("./api/get");
 
 const app = express();
+app.use(cors()); // Use this after the variable declaration
 
 // Use body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors()); // Use this after the variable declaration
+app.use(express.static(__dirname + "/node_modules/socket.io/client-dist/"));
 
 // Connect to MongoDB using Mongoose
-// This will be implemeted shortly
+// This will be implemented shortly
 
 // Create HTTP server and attach Socket.io
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Handle socket connection
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
   // Handle incoming messages from the client
-  socket.on('lamp:on', (data) => {
-    console.log('Received lamp:on message');
+  socket.on("lamp:on", (data) => {
+    console.log("Received lamp:on message");
     // Send a message back to the client to acknowledge the request
-    socket.emit('lamp:status', { status: 'on' });
+    socket.emit("lamp:status", { status: "on" });
   });
 
-  socket.on('lamp:off', (data) => {
-    console.log('Received lamp:off message');
+  socket.on("lamp:off", (data) => {
+    console.log("Received lamp:off message");
     // Send a message back to the client to acknowledge the request
-    socket.emit('lamp:status', { status: 'off' });
+    socket.emit("lamp:status", { status: "off" });
   });
 
   // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
   });
 });
 
 // Use the GET router
-app.use("/", getRouter(io));
+const router = getRoutes(io);
+app.use("/", router);
 
 // Start the server
 const port = process.env.PORT || 3000;
 server.listen(port, function () {
   console.log(`Server is listening on port ${port}`);
 });
-
